ef74180dacdb772032a45bcf1dcf0306f79306ba
kumaradarsh50 <kumar.adarsh50@gmail.com>
2022-12-26 22:08:34 +0530

    maze solver

---
+++ src/main/java/com/mycompany/maze_solver/Maze_solver.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.maze_solver;
+
+/**
+ *
+ * @author adarsh
+ */
+public class Maze_solver {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
